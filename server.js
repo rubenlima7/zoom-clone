45fcaecf94814a5aafeb9c1e5e137070a7590c35
rubenlima7 <rubenlima7@users.noreply.github.com>
2020-08-10 00:19:27 +0100

    Fist Commit

---
+++ server.js
@@ -0,0 +1,46 @@
+/*!
+ *  Zoom Clone
+ *   (c) 2020 Ruben Lima
+ */
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const { v4: uuidv4 } = require('uuid');
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use('/peerjs', peerServer);
+app.get('/', (request, response) => {
+  response.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (request, response) => {
+  response.render('room', { roomId: request.params.room })
+})
+
+
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit('user-connected', userId);
+
+    socket.on('disconnect', () => {
+      socket.to(roomId).broadcast.emit('user-disconnected', userId);
+    })
+
+    socket.on('message', message => {
+
+      io.to(roomId).emit('createMessage', message, userId);
+    });
+  })
+})
+
+server.listen(process.env.PORT || 3333);
